780557e2311fefc8049ab2e159865857f81ee818
Lukasz Skraba <student@it11-10.it.local>
2022-11-10 12:32:24 +0100

    Wersja 1

---
+++ projektgit1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double a, b, mz;
+	cout<<"Podaj a: ";
+	cin>>a;
+	cout<<"Podaj b: ";
+	cin>>b;
+	cout<<endl;
+	mz=-b/a;	
+	
+	cout<<"Miejsce zerowe funkcji liniowej: "<<mz;
+	/*
+	if (a!=0)
+	{
+		cout<<"Miejsce zerowe funkcji liniowej: "<<mz;
+	}
+	else 
+		cout<<"Brak miejsc zerowych";
+	
+	*/
+	
+	return 0;
+}

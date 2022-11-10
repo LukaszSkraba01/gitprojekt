d81b484dc15c8ab8ff02e271fd7e22fc3cbd963f
Lukasz Skraba <student@it11-10.it.local>
2022-11-10 12:39:23 +0100

    Wersja 2

--- projektgit1.cpp
+++ projektgit1.cpp
@@ -12,8 +12,7 @@ int main() {
 	cout<<endl;
 	mz=-b/a;	
 	
-	cout<<"Miejsce zerowe funkcji liniowej: "<<mz;
-	/*
+	
 	if (a!=0)
 	{
 		cout<<"Miejsce zerowe funkcji liniowej: "<<mz;
@@ -21,7 +20,6 @@ int main() {
 	else 
 		cout<<"Brak miejsc zerowych";
 	
-	*/
 	
 	return 0;
 }
